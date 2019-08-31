e098d6883bc52ae5b8edbb269be23d67b946d19e
Bert Keller <bertwinkeller@gmail.com>
2019-08-30 18:27:14 -0700

    created variable in js file, added html elements

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Defining variables for the game 
+
+var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+var remainingGuesses = 10
+
+var hipHopArtists = ['notoriousbig','tupac','mobbdeep','eminem','tribecalledquest','snoopdog','gangstarr','wutangclan']
+
+var emptyUnderscore = []
+
+// Creates variables to hold place for places we defined in out HTML
