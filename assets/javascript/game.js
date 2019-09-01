a17eab9d564834f5397073b2bc595bfdda820ca5
Bert Keller <bertwinkeller@gmail.com>
2019-08-31 17:30:57 -0700

    continued work with js functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,8 @@
 // Defining variables for the game 
 
-var letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-
 var remainingGuesses = 10
 
 var hipHopArtists = ['notoriousbig','tupac','mobbdeep','eminem','tribecalledquest','snoopdog','gangstarr','wutangclan']
-
-var emptyUnderscore = []
 
 var randomNumberGenerator = [Math.floor(Math.random() * hipHopArtists.length)];
 
@@ -16,28 +12,45 @@ var chosenArtist= hipHopArtists[randomNumberGenerator]
 
 // Creates variables to hold place for places we defined in out HTML
 
-var directionsText = document.getElementById('directions-text');
+var directionsText = document.getElementById("directions-text");
 
-var currentWord = document.getElementById('current-word');
+var currentWord = document.getElementById("current-word");
 
-var guessingLines = document.getElementById('guessing-lines');
+var guessingLines = document.getElementById("guessing-lines");
 
-var guessesRemainingText = document.getElementById('number-guess-remain');
+var guessesRemainingText = document.getElementById("number-guess-remain");
 
-var letterGuessedText = document.getElementById('letters-already-guessed');
+var letterGuessedText = document.getElementById("letters-already-guessed");
+
+
+// variable sets empty string
+var generateLines = ''
+var rightWord = ''
+
+// function that generates underscores
+
+function underscoreGenerator () {
+    for (let i=0; i < chosenArtist.length; i++){
+    generateLines += ' _ ';
+    } return generateLines;
+}
+
+// creates underscores on html page 
+guessingLines.textContent = underscoreGenerator()
+
+
 
  // This function is run whenever the user presses a key.
  document.onkeyup = function(event) {
 
- // Determines which key was pressed.
- var userLetter = event.key;
- 
+
+// makes variable for 
+// Determines which key was pressed.
+ var userLetter = String.fromCharCode(event.key);
+
  for(let i=0; i<chosenArtist.length; i++){
-if (i===userLetter){
-
-
-
+     if(userLetter === i){
+rightWord += userLetter
+     }
+ }
 }
-
- }
- }
