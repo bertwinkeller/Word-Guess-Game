e625c81c64aaa0544db58d3b8e74bd105cdc6b74
Bert Keller <bertwinkeller@gmail.com>
2019-09-02 17:36:20 -0700

    restructured functions for entire game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,65 +1,85 @@
 // Defining variables for the game 
 
+
+
+var wordBank = ['notoriousbig','tupac','mobbdeep','eminem','tribecalledquest','snoopdog','gangstarr','wutangclan']
+
+var randomNumberGenerator = [Math.floor(Math.random() * wordBank.length)];
+
+var chosenArtist= wordBank[randomNumberGenerator]
+
 var remainingGuesses = 10
 
-var hipHopArtists = ['notoriousbig','tupac','mobbdeep','eminem','tribecalledquest','snoopdog','gangstarr','wutangclan']
+var wins = 0
 
-var randomNumberGenerator = [Math.floor(Math.random() * hipHopArtists.length)];
+var letters = []
 
-var chosenArtist= hipHopArtists[randomNumberGenerator]
+var wrongLetters = []
 
-
-
-// Creates variables to hold place for places we defined in out HTML
-
-var directionsText = document.getElementById("directions-text");
-
-var currentWord = document.getElementById("current-word");
-
-var guessingLines = document.getElementById("guessing-lines");
-
-var guessesRemainingText = document.getElementById("number-guess-remain");
-
-var letterGuessedText = document.getElementById("letters-already-guessed");
+var output = []
 
 
-// variable sets empty string
-var generateLines = ''
+function gameStart() {
+
+remainingGuesses = 10
+wrongLetters = [];
+output = [];
 
 
-// function that generates underscores
-
-function underscoreGenerator () {
-    for (let i=0; i < chosenArtist.length; i++){
-    generateLines += ' _ ';
-    } return generateLines;
+for (let i =0;i < chosenArtist.length; i++){
+    output.push(" _ ");
+    console.log(output)
+   
 }
 
 
+var guessingLines = document.getElementById("guessing-lines")
+guessingLines.textContent = output.join(" ")
 
-// creates underscores on html page 
-guessingLines.textContent = underscoreGenerator()
+var numGuessRemain = document.getElementById("number-guess-remain")
+numGuessRemain.textContent = "Number of guesses remaining: " + remainingGuesses
 
+var lettersGuessed = document.getElementById("letters-already-guessed")
+lettersGuessed.textContent = "Letters already guessed: " + wrongLetters.join(" ")
 
-// function to update with guesses 
-
- // This function is run whenever the user presses a key.
- document.onkeyup = function(event) {
-
-
-// makes variable for 
-// Determines which key was pressed.
-var userLetter = event.key;
-
-
- for(let i=0; i<chosenArtist.length; i++){
-  if(chosenArtist[i] === userLetter){
-      guessingLines.textContent = guessingLines.textContent.replace('_', chosenArtist[i]);
-  }else{
-  }
-     }
-
- }
+};
+gameStart()
+// // variable sets empty string
+// var generateLines = ''
 
 
-   
+// // function that generates underscores
+
+// function underscoreGenerator () {
+//     for (let i=0; i < chosenArtist.length; i++){
+//     generateLines += ' _ ';
+//     } return generateLines;
+// }
+
+
+
+// // creates underscores on html page 
+// guessingLines.textContent = underscoreGenerator()
+
+
+// // function to update with guesses 
+
+//  // This function is run whenever the user presses a key.
+//  document.onkeyup = function(event) {
+
+
+// // makes variable for 
+// // Determines which key was pressed.
+// var userLetter = event.key;
+
+
+//  for(let i=0; i<chosenArtist.length; i++){
+//   if(chosenArtist[i] === userLetter){
+//       guessingLines.textContent = guessingLines.textContent.replace('_', chosenArtist[i]);
+//   }else{
+//   }
+//      }
+
+//  }
+
+
